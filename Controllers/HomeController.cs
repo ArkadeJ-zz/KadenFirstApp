89f90abed853a9b0e17dd85133fecf0f21840ca5
ArkadeJ <kadenjohnson@gmail.com>
2021-01-27 12:18:52 -0700

    Update

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using KadenFirstApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KadenFirstApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("GradeCalc")]
+        public IActionResult GradeCalc()
+        {
+            return View();
+        }
+
+        [HttpPost("GradeCalc")]
+        public IActionResult GradeCalc(GradesModel model)
+        {
+            return View();//Go in and fix the submit function
+        }
+    }
+}
---
+++ Models/GradesModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KadenFirstApp.Models
+{
+    public class GradesModel{
+        [Required]
+        [Range(0, 100, ErrorMessage = "Grade Percentage must be between 0% and 100%")]
+        public int Assignments { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Grade Percentage must be between 0% and 100%")]
+        public int Groupprojects { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Grade Percentage must be between 0% and 100%")]
+        public int Quizzes { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Grade Percentage must be between 0% and 100%")]
+        public int Exams { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Grade Percentage must be between 0% and 100%")]
+        public int Intex { get; set; }
+    }
+}
